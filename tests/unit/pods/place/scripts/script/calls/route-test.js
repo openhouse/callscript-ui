eee694eefcff16827452fbd7d3030c29c571d4ee
Jamie Burkart <jamie@wowlist.org>
2016-12-02 23:49:52 -0500

    break out into routes

---
+++ tests/unit/pods/place/scripts/script/calls/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:place/scripts/script/calls', 'Unit | Route | place/scripts/script/calls', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
